f83776fd0d718088be0ae721f2734677270f0820
priyanshu219 <pgarg1@cs.iitr.ac.in>
2020-01-19 21:15:05 +0530

    Successfully detected cipher

--- Set2/[3]An_ECBCBC_detection_oracle/Challenge.py
+++ Set2/[3]An_ECBCBC_detection_oracle/Challenge.py
@@ -1,7 +1,9 @@
 from random import randint
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 mode='NONE'
+key=b''
 block_size = 16
 
 def pad(txt):
@@ -15,19 +17,8 @@ def pad(txt):
 def Xor(a, b):
 	xored=b''
 	for i in range(0,len(a)):
-		xored += chr(ord(a[i])^ord(b[i]))
-	return xored.encode()
-
-
-def create_random(length):
-	txt=''
-	for i in range(0, length):
-		txt += chr(randint(0, 256))
-
-	print("text:", len(txt))
-	print("txt encode : ", len(txt.encode()))
-
-	return txt.encode()
+		xored += bytes([a[i]^b[i]])
+	return xored
 
 def AES_128_ECB_encrypt(txt, key):
 	cipher = AES.new(key, AES.MODE_ECB)
@@ -35,7 +26,7 @@ def AES_128_ECB_encrypt(txt, key):
 	return encrypted
 
 def AES_128_CBC_encrypt(txt, key):
-	init_vec = create_random(block_size);
+	init_vec = get_random_bytes(block_size);
 	encrypted=b''
 	encrypted = AES_128_ECB_encrypt(Xor(txt[0:16], init_vec), key)
 	for i in range(16, len(txt), 16):
@@ -52,10 +43,10 @@ def is_ECB(cipher):
 	return False
 
 def encryption_oracle(txt):
+	global mode
+	global key
 	txt = pad(txt)
-	key = create_random(block_size).decode()
-	print("Key size : ", len(key))
-	print(key)
+	key = get_random_bytes(block_size)
 	cipher=b''
 	if(randint(0, 2)):
 		cipher = AES_128_ECB_encrypt(txt, key)
@@ -65,7 +56,6 @@ def encryption_oracle(txt):
 		cipher = AES_128_CBC_encrypt(txt, key)
 		mode='CBC'
 
-	print(cipher)
 	return cipher
 
 def identify_encryptionmode(cipher):
@@ -77,17 +67,19 @@ def identify_encryptionmode(cipher):
 
 
 def main():
-	txt = create_random(50) + b'A'*(randint(4, 56))+ b'A'*(randint(1, 45)) + create_random(78)
-	txt = create_random(randint(5, 11)) + txt + create_random(randint(5, 11))
+	txt = get_random_bytes(50) + b'A'*(randint(4, 56))+ b'A'*(randint(1, 45)) + get_random_bytes(78)
+	txt = get_random_bytes(randint(5, 11)) + txt + get_random_bytes(randint(5, 11))
 	
 	print("-----Original Message----")
 	print(txt)
 	print()
 	print("-----Encrypted Message----")
 	encryption = encryption_oracle(txt)
-	print()
+	print(encryption)
 	if(mode==identify_encryptionmode(encryption)):
 		print("Yahoo detected")
+		print("Encrypted mode is : ", mode)
+		print("Encryption key : ", key)
 	else:
 		print("Better luck next time")
 
